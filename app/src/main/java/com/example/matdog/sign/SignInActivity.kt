42e6f090a486c8ad3a53cca9fe8ffe3e4481c241
eunnj <yenje7@naver.com>
2020-06-07 09:56:17 +0900

    sign in/sign up

--- app/src/main/java/com/example/matdog/sign/SignInActivity.kt
+++ app/src/main/java/com/example/matdog/sign/SignInActivity.kt
@@ -1,13 +1,20 @@
 package com.example.matdog.sign
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.matdog.R
+import kotlinx.android.synthetic.main.activity_sign_in.*
 
 class SignInActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        btn_signup.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/matdog/sign/SignUpActivity.kt
+++ app/src/main/java/com/example/matdog/sign/SignUpActivity.kt
@@ -1,13 +1,21 @@
 package com.example.matdog.sign
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.matdog.R
+import kotlinx.android.synthetic.main.activity_sign_in.*
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        btn_prev.setOnClickListener {
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
