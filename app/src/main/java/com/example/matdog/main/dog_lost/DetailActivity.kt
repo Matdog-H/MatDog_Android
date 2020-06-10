2e30820eef56d45f182f2e41b83170f3c11ce48c
ksb0511 <soobin796@gmail.com>
2020-06-10 22:23:31 +0900

    detail -ing

--- app/src/main/java/com/example/matdog/main/dog_lost/DetailActivity.kt
+++ app/src/main/java/com/example/matdog/main/dog_lost/DetailActivity.kt
@@ -1,13 +1,41 @@
 package com.example.matdog.main.dog_lost
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
 import com.example.matdog.R
+import com.example.matdog.main.dog_lost.detail_popup.Detail_popupActivity
+import kotlinx.android.synthetic.main.activity_detail.*
+
 
 class DetailActivity : AppCompatActivity() {
+    internal lateinit var viewpager : ViewPager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
+        viewpager = findViewById(R.id.pager) as ViewPager
+
+        val adapter = ViewPagerAdapter(this)
+        viewpager.adapter = adapter
+
+        init()
+    }
+
+    private fun init(){
+        btn_detail_call.setOnClickListener {
+            val i = Intent(this, Detail_popupActivity::class.java)
+            startActivity(i)
+        }
+
+        btn_detail_back.setOnClickListener {
+            finish()
+        }
     }
 }
---
+++ app/src/main/java/com/example/matdog/main/dog_lost/ViewPagerAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.matdog.main.dog_lost
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.example.matdog.R
+
+/** @author Aidan Follestad (afollestad) */
+class ViewPagerAdapter(private val context : Context) : PagerAdapter() {
+
+    private var layoutInflater : LayoutInflater? = null
+    val Image = arrayOf(
+        R.drawable.taepoong3,
+        R.drawable.taepoong2,
+        R.drawable.taepoong
+    )
+
+
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+        return view ===  `object`
+    }
+
+    override fun getCount(): Int {
+        return Image.size
+    }
+
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        layoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val v = layoutInflater!!.inflate(R.layout.detail_photo_item, null)
+        val image = v.findViewById<View>(R.id.imageView) as ImageView
+
+        image.setImageResource(Image[position])
+        val vp = container as ViewPager
+        vp.addView(v , 0)
+
+
+        return v
+
+    }
+
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        val vp = container as ViewPager
+        val v = `object` as View
+        vp.removeView(v)
+    }
+}
