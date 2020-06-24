74e41b3086f3f22ccb589e13109c7d9695cb9a2d
eunnj <yenje7@naver.com>
2020-06-24 15:01:53 +0900

    팝업창

--- app/src/main/java/com/example/matdog/main/dog_list/detail_list_popup/Detail_list_popupActivity.kt
+++ app/src/main/java/com/example/matdog/main/dog_list/detail_list_popup/Detail_list_popupActivity.kt
@@ -2,12 +2,23 @@ package com.example.matdog.main.dog_list.detail_list_popup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.matdog.R
+import kotlinx.android.synthetic.main.activity_detail_list_popup.*
 
 class Detail_list_popupActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_list_popup)
+
+        editbutton.setOnClickListener(View.OnClickListener {
+            //수정
+
+            //나가기
+            finish()
+        })
     }
+
+
 }
