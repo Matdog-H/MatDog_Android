309908fdf3df9f17a353f8dfb7d771d6acbd12f7
nhj7911 <nhj7911@gmail.com>
2020-08-15 21:45:29 +0900

    마이페이지 프로필 디자인 및 크기 수정

--- app/src/main/java/com/example/matdog/main/dog_shelter/Write_Shelter_Activity.kt
+++ app/src/main/java/com/example/matdog/main/dog_shelter/Write_Shelter_Activity.kt
@@ -9,11 +9,14 @@ package com.example.matdog.main.dog_shelter
 import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.ClipData
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Toast
 import com.example.matdog.R
 import com.example.matdog.main.pop_up.Renew_popupActivity
@@ -21,6 +24,8 @@ import kotlinx.android.synthetic.main.activity_write.*
 import kotlinx.android.synthetic.main.activity_write.ic_back
 
 class Write_Shelter_Activity : AppCompatActivity() {
+
+    final val PICTURE_REQUEST_CODE = 100;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,58 +58,38 @@ class Write_Shelter_Activity : AppCompatActivity() {
     private fun picture() {
         //Change profile Image
         picture_write1_plus.setOnClickListener {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
-                    PackageManager.PERMISSION_DENIED
-                ) {
-
-                    val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
-                    requestPermissions(permissions,
-                        PERMISSION_CODE
-                    )
-                } else {
-                    pickImageFromGallery()
-                }
-            }
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Audio.Media.EXTERNAL_CONTENT_URI)
+            // 사진 여러개 선택
+            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE,true)
+            intent.setType("Image/*")
+            startActivityForResult(Intent.createChooser(intent,"Select picture"),PICTURE_REQUEST_CODE) // 액티비티의 결과를 받기위한..
         }
     }
 
-    //Change profile image
-    private fun pickImageFromGallery(){
-        val intent = Intent(Intent.ACTION_PICK)
-        intent.type = "image/*"
-        startActivityForResult(intent,
-            IMAGE_PICK_CODE
-        )
-    }
-
-    companion object{
-        private val IMAGE_PICK_CODE = 1000
-        private val PERMISSION_CODE = 1001
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        when(requestCode){
-            PERMISSION_CODE -> {
-                if (grantResults.size > 0 && grantResults[0] ==
-                    PackageManager.PERMISSION_GRANTED){
-                    pickImageFromGallery()
-                }
-                else{
-                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
-    }
-
-    @SuppressLint("MissingSuperCall")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if(resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
-            picture_write1.setImageURI(data?.data)
+        if(resultCode == PICTURE_REQUEST_CODE ){
+          if(resultCode == Activity.RESULT_OK){
+              // 기존 이미지지우기
+              picture_write1.setImageResource(0)
+              picture_write2.setImageResource(0)
+              picture_write3.setImageResource(0)
+
+              // ClipData 또는 URI를 가져옴
+              var currentImageUri : Uri? = data?.data
+
+              try{
+                  for(i in 0..2){
+                      when(i){
+                          0 -> picture_write1.setImageURI(currentImageUri)
+                          1 -> picture_write2.setImageURI(currentImageUri)
+                          2 -> picture_write3.setImageURI(currentImageUri)
+                      }
+                  }
+              }catch(e:Exception){
+                  e.printStackTrace()
+              }
+
+          }
         }
     }
 }
