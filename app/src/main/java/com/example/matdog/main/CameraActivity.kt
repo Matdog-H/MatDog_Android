6865ae2c9e8667cafd2557960701fb1084877202
nhj7911 <nhj7911@gmail.com>
2020-08-16 14:35:47 +0900

    공고등록 앨범에서 사진 가져오기 진행중

--- app/src/main/java/com/example/matdog/main/CameraActivity.kt
+++ app/src/main/java/com/example/matdog/main/CameraActivity.kt
@@ -27,7 +27,7 @@ class CameraActivity : AppCompatActivity() {
     }
 
     private fun picture() {
-        //Change profile Image
+        // 앨범(갤러리에서) 사진 가져오기
         get_photo.setOnClickListener {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
@@ -45,7 +45,7 @@ class CameraActivity : AppCompatActivity() {
         }
     }
 
-    //Change profile image
+    //앨범에서 이미지 가져오기
     private fun pickImageFromGallery(){
         val intent = Intent(Intent.ACTION_PICK)
         intent.type = "image/*"
--- app/src/main/java/com/example/matdog/main/dog_shelter/Write_Shelter_Activity.kt
+++ app/src/main/java/com/example/matdog/main/dog_shelter/Write_Shelter_Activity.kt
@@ -19,77 +19,103 @@ import android.os.Bundle
 import android.provider.MediaStore
 import android.widget.Toast
 import com.example.matdog.R
+import com.example.matdog.main.CameraActivity
 import com.example.matdog.main.pop_up.Renew_popupActivity
+import kotlinx.android.synthetic.main.activity_camera.*
 import kotlinx.android.synthetic.main.activity_write.*
 import kotlinx.android.synthetic.main.activity_write.ic_back
 
 class Write_Shelter_Activity : AppCompatActivity() {
 
-    final val PICTURE_REQUEST_CODE = 100;
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_write)
 
-        ic_back.setOnClickListener {
+        ic_back.setOnClickListener { // 뒤로가기 버튼 눌렀을 때
             finish()
         }
 
-        btn_okwrite.setOnClickListener {
+        btn_okwrite.setOnClickListener { // 등록하기 버튼 눌렀을 때
             finish()
         }
 
-        species_modify.setOnClickListener {
-            species_name.isEnabled = true // 종 수정하기
+        species_modify.setOnClickListener { // 종 수정버튼 눌렀을 때,
+            // 종 수정가능해짐
+            species_name.isEnabled = true
+        }
+
+        radionotouch.setOnClickListener { // 연락처수정 라디오버튼을 눌렀을 때,
+            // 연락처수정할 수 있는 팝업창 띄움
+            val i = Intent(this, Renew_popupActivity::class.java)
+            startActivity(i)
+
         }
 
         picture()
 
-
-        radionotouch.setOnClickListener {
-            val i = Intent(this, Renew_popupActivity::class.java)
-            startActivity(i)
-
-//            val intent = Intent(this@ListActivity, WriteActivity::class.java)
-//            startActivity(intent)
-        }
     }
 
     private fun picture() {
-        //Change profile Image
-        picture_write1_plus.setOnClickListener {
-            val intent = Intent(Intent.ACTION_PICK, MediaStore.Audio.Media.EXTERNAL_CONTENT_URI)
-            // 사진 여러개 선택
-            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE,true)
-            intent.setType("Image/*")
-            startActivityForResult(Intent.createChooser(intent,"Select picture"),PICTURE_REQUEST_CODE) // 액티비티의 결과를 받기위한..
+        // 앨범(갤러리에서) 사진 가져오기
+        get_photo.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                    PackageManager.PERMISSION_DENIED
+                ) {
+
+                    val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    requestPermissions(permissions,
+                        PERMISSION_CODE
+                    )
+                } else {
+                    pickImageFromGallery()
+                }
+            }
         }
     }
 
+    // 앨범에서 이미지 가져오기
+    private fun pickImageFromGallery(){
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE,true) // 여러장 선택가능
+        intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+    }
+
+    companion object{
+        private val IMAGE_PICK_CODE = 1000
+        private val PERMISSION_CODE = 1001
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode){
+            PERMISSION_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED){
+                    pickImageFromGallery()
+                }
+                else{
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    @SuppressLint("MissingSuperCall")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if(resultCode == PICTURE_REQUEST_CODE ){
-          if(resultCode == Activity.RESULT_OK){
-              // 기존 이미지지우기
-              picture_write1.setImageResource(0)
-              picture_write2.setImageResource(0)
-              picture_write3.setImageResource(0)
+        if(resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
+            if(data == null){
 
-              // ClipData 또는 URI를 가져옴
-              var currentImageUri : Uri? = data?.data
+            }else{
 
-              try{
-                  for(i in 0..2){
-                      when(i){
-                          0 -> picture_write1.setImageURI(currentImageUri)
-                          1 -> picture_write2.setImageURI(currentImageUri)
-                          2 -> picture_write3.setImageURI(currentImageUri)
-                      }
-                  }
-              }catch(e:Exception){
-                  e.printStackTrace()
-              }
+            }
 
-          }
+            picture_button1.setImageURI(data?.data)
         }
     }
 }
