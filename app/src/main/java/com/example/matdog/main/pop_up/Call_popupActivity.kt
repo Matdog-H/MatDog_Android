5b9abf00f646edf2f39818cef3e4278ee961c2b9
nhj7911 <nhj7911@gmail.com>
2020-09-14 23:47:36 +0900

    공고 상세화면 수정

--- app/src/main/java/com/example/matdog/main/pop_up/Call_popupActivity.kt
+++ app/src/main/java/com/example/matdog/main/pop_up/Call_popupActivity.kt
@@ -9,17 +9,14 @@ import kotlinx.android.synthetic.main.activity_detail_list_popup.*
 import kotlinx.android.synthetic.main.activity_detail_popup.*
 
 class Call_popupActivity : AppCompatActivity() {
-
+    // 공고 상세화면 연락처 팝업
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_popup)
 
         closebutton.setOnClickListener(View.OnClickListener {
-            // editText란이 하나라도 정보가 기입되지 않은 경우
-            if(list_popup_tel.equals("") || list_popup_dm.equals("") || list_popup_email.equals(""))
-                Toast.makeText(this, "모든 연락정보를 입력해주세요.",Toast.LENGTH_LONG)
-            else //나가기
-                finish()
+            //나가기
+            finish()
         })
     }
 }
--- app/src/main/java/com/example/matdog/main/pop_up/Renew_popupActivity.kt
+++ app/src/main/java/com/example/matdog/main/pop_up/Renew_popupActivity.kt
@@ -2,21 +2,33 @@ package com.example.matdog.main.pop_up
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import com.example.matdog.R
 import kotlinx.android.synthetic.main.activity_detail_list_popup.*
+import kotlinx.android.synthetic.main.activity_sign_in.*
 
 class Renew_popupActivity : AppCompatActivity() {
-
+    // 공고등록 연락처 수정 팝업
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_list_popup)
 
-        editbutton.setOnClickListener(View.OnClickListener {
+        popup_editbutton.setOnClickListener(View.OnClickListener {
             //수정
+            // editText란이 하나라도 정보가 기입되지 않은 경우 토스트 띄우기
+            var tel = list_popup_tel.text.toString()
+            var email = list_popup_email.text.toString()
+            var dm = list_popup_dm.text.toString()
 
-            //나가기
-            finish()
+            if(tel.equals(""))
+                Toast.makeText(this, "모든 연락정보를 입력해주세요.", Toast.LENGTH_LONG).show()
+            else if(email.equals(""))
+                Toast.makeText(this, "모든 연락정보를 입력해주세요.", Toast.LENGTH_LONG).show()
+            else if (dm.equals(""))
+                Toast.makeText(this, "모든 연락정보를 입력해주세요.", Toast.LENGTH_LONG).show()
+            else finish()
         })
     }
 
