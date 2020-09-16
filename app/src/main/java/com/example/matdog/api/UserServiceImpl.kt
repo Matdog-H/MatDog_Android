c1e6aca7186797f0cd7784618b3e6e553b087a45
eunnj <yenje7@naver.com>
2020-09-16 16:39:59 +0900

    마이페이지 리사이클러뷰 분리

--- app/src/main/java/com/example/matdog/api/UserServiceImpl.kt
+++ app/src/main/java/com/example/matdog/api/UserServiceImpl.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object UserServiceImpl {
-    private const val BASE_URL = "http://3.34.224.42:8081"
+    private const val BASE_URL = "http://3.34.224.42:8080"
 
     private val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
@@ -13,4 +13,6 @@ object UserServiceImpl {
 
     val userService: UserService = retrofit.create(UserService::class.java)
     val ListService: ListInter = retrofit.create(ListInter::class.java)
+    //val SignupService: SignupInter = retrofit.create(SignupInter::class.java)
+    //val IdCheckService: IdCheckInter = retrofit.create(IdCheckInter::class.java)
 }
