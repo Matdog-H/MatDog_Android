73d2829e7f0350d0df04d7771c029a7079ea549e
eunnj <yenje7@naver.com>
2020-09-20 16:42:42 +0900

    마이페이지 - 찜 공고보기

--- app/src/main/java/com/example/matdog/main/mypage/fragment_like.kt
+++ app/src/main/java/com/example/matdog/main/mypage/fragment_like.kt
@@ -10,20 +10,55 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.matdog.R
+import com.example.matdog.api.SharedPreferenceController
+import com.example.matdog.api.UserServiceImpl
+import com.example.matdog.api.safeEnqueue
 import com.example.matdog.main.Share_files.Recyclerview_share.rv_Adapter
 import com.example.matdog.main.Share_files.Recyclerview_share.ListItem
 import com.example.matdog.main.dog_protect.Detail_Protect_Activity
 import kotlinx.android.synthetic.main.list_item.*
 
 class fragment_like : Fragment(), View.OnClickListener{
-
+    private var token : String = ""
     private lateinit var  FLrecyclerview : RecyclerView
     private var mpadapter1: mp_Adapter = mp_Adapter(R.layout.list_item)
+    var mp_datalist = ArrayList<ArrayList<ListItem>>()
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         var like_listview=inflater.inflate(R.layout.like_list, container, false)
         var thiscontext = container!!.getContext()
+
+        //-------------server-----------------
+        token = SharedPreferenceController.getUserToken(thiscontext)
+        val calllikepost = UserServiceImpl.MyListService.listResponse_mylike(token)
+        calllikepost.safeEnqueue {
+            if(it.isSuccessful){
+                val LikePost = it.body()!!.mylistdata
+                var my_like_list = arrayListOf<ListItem>()
+                for(i in 0 until LikePost.size){
+                    my_like_list.add(
+                       ListItem(
+                           it_species = LikePost[i].kindCd,
+                           it_status = LikePost[i].registerStatus,
+                           it_gender = LikePost[i].sexCd,
+                           it_age = LikePost[i].age,
+                           it_date = LikePost[i].happenDt,
+                           it_image = LikePost[i].filename
+                       )
+                   )
+                }
+
+               mpadapter1.mp_data = my_like_list
+               mpadapter1.notifyDataSetChanged()
+               mp_datalist.add(mpadapter1.mp_data)
+
+           }
+       }
+
+        //-------------server-----------------
+
 
         FLrecyclerview = like_listview.findViewById(R.id.ll_recyclerview)
 
@@ -39,44 +74,7 @@ class fragment_like : Fragment(), View.OnClickListener{
         //리사이클러뷰 배치
         FLrecyclerview.layoutManager= GridLayoutManager(thiscontext,2)
 
-//        mpadapter1.mp_data= arrayListOf(
-//            ListItem(
-//                it_image = "1",
-//                it_species = "말티즈",
-//                it_age = "3",
-//                it_status = R.drawable.state_shelter,
-//                it_gender = "1",
-//                it_date = "2020.06.12"
-//            ), ListItem(
-//                it_image = "1",
-//                it_species = "말티즈",
-//                it_age = "3",
-//                it_status = R.drawable.state_shelter,
-//                it_gender = "1",
-//                it_date = "2020.06.12"
-//            ), ListItem(
-//                it_image = "1",
-//                it_species = "말티즈",
-//                it_age = "3",
-//                it_status = R.drawable.state_shelter,
-//                it_gender = "1",
-//                it_date = "2020.06.12"
-//            ), ListItem(
-//                it_image = "1",
-//                it_species = "말티즈",
-//                it_age = "3",
-//                it_status = R.drawable.state_shelter,
-//                it_gender = "1",
-//                it_date = "2020.06.12"
-//            ), ListItem(
-//                it_image = "1",
-//                it_species = "말티즈",
-//                it_age = "3",
-//                it_status = R.drawable.state_shelter,
-//                it_gender = "1",
-//                it_date = "2020.06.12"
-//            )
-//        )
+
 
         mpadapter1.notifyDataSetChanged()
 
