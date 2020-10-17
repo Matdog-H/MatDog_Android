ed51c79bea4aac220dbc273db62d0dc7336f7a67
nhj7911 <nhj7911@gmail.com>
2020-10-17 16:22:58 +0900

    실종,임시보호공고 등록할시 리스트 상태 바뀌도록

--- app/src/main/java/com/example/matdog/main/Share_files/List_share/List_Activity.kt
+++ app/src/main/java/com/example/matdog/main/Share_files/List_share/List_Activity.kt
@@ -98,7 +98,9 @@ class List_Activity : AppCompatActivity() {
                 if (search_data == null) {
                     var fragmentAdapter =
                         ViewPager_Shelter_Adapter(
-                            supportFragmentManager, state_result, " " // 상태값에 따라 서버연결 리스트가 변하도록
+                            supportFragmentManager,
+                            state_result, // 상태값에 따라 서버연결 리스트가 변하도록
+                            " "
                         )
                     list_viewPager.adapter = fragmentAdapter
                     list_tablayout.setupWithViewPager(list_viewPager)
@@ -106,8 +108,8 @@ class List_Activity : AppCompatActivity() {
                     var fragmentAdapter =
                         ViewPager_Shelter_Adapter(
                             supportFragmentManager,
-                            state_result,
-                            search_data // 상태값에 따라 서버연결 리스트가 변하도록
+                            state_result,  // 상태값에 따라 서버연결 리스트가 변하도록
+                            search_data
                         )
                     list_viewPager.adapter = fragmentAdapter
                     list_tablayout.setupWithViewPager(list_viewPager)
@@ -121,21 +123,22 @@ class List_Activity : AppCompatActivity() {
             var fragmentAdapter =
                 ViewPager_Shelter_Adapter(
                     supportFragmentManager,
-                    state_result - 3,
-                    dog_breed // 상태값에 따라 서버연결 리스트가 변하도록
+                    state_result - 3,  // 상태값에 따라 서버연결 리스트가 변하도록
+                    dog_breed
                 )
             list_viewPager.adapter = fragmentAdapter
             list_tablayout.setupWithViewPager(list_viewPager)
         }
 
-        // 글등록 버튼
+        // 글등록 버튼(+)
         btn_write.setOnClickListener {
             if (state_result == 0 || state_result == 3) {
                 val intent1 = Intent(this, Write_Shelter_Activity::class.java)
                 startActivityForResult(intent1, 2000)
-            } else if (state_result == 1 || state_result == 4) {
-                val intent2 = Intent(this, Write_Miss_Activity::class.java)
+            } else if (state_result == 1 || state_result == 4) { // 임시보호리스트(전체)
+                val intent2 = Intent(this, Write_Miss_Activity::class.java) // 실종공고등록창
                 startActivityForResult(intent2, 3000)
+                // 실종공고리스트가 뜨도록..상태값 바꿔줘야함..
             } else {
                 val intent3 = Intent(this, Write_Protect_Activity::class.java)
                 startActivityForResult(intent3, 4000)
@@ -149,13 +152,15 @@ class List_Activity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == 2000 || requestCode == 3000 || requestCode == 4000) {
             Log.v("requestCode로 들어옴", "requestCode로 들어왔음")
+            // 분양공고보기
             var a1 = intent.getStringExtra("state0") // 보호소 리스트 - 분양공고등록
-            var a2 = intent.getStringExtra("state1") // 임시보호 리스트 - 실종공고등록
-            var a3 = intent.getStringExtra("state2") // 실종 리스트 - 임시보호공고등록
+            var a2 = intent.getStringExtra("state2") // 임시보호 리스트 - 실종공고등록
+            var a3 = intent.getStringExtra("state1") // 실종 리스트 - 임시보호공고등록
+            // 추천공고보기(품종 분석결과값 기반으로)
             var a4 = intent.getStringExtra("state3") // 보호소 리스트 - 품종결과
-            var a5 = intent.getStringExtra("state4") // 임시보호 리스트 - 품종결과
-            var a6 = intent.getStringExtra("state5") // 실종 리스트 - 품종결과
-
+            var a5 = intent.getStringExtra("state5") // 임시보호 리스트 - 품종결과
+            var a6 = intent.getStringExtra("state4") // 실종 리스트 - 품종결과
+            // 품종 분석결과값
             var dog_breed_register = intent.getStringExtra("breed1") // 보호소 리스트 - 분석결과값
             var dog_breed_protect = intent.getStringExtra("breed2") // 임시보호 리스트 - 분석결과값
             var dog_breed_lost = intent.getStringExtra("breed3") // 실종 리스트 - 분석결과값
@@ -182,8 +187,8 @@ class List_Activity : AppCompatActivity() {
                     break
                 }
             }
-
             init()
+        } else if(requestCode==3000){
 
         }
     }
